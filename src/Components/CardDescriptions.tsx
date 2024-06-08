44cec8b57e8decb38fd41ec2daedcb90d52d1b31
unknown <faisaljawed2000@gmail.com>
2024-06-09 00:44:11 +0500

    add Card Desc styleing

--- src/Components/CardDescriptions.tsx
+++ src/Components/CardDescriptions.tsx
@@ -26,13 +26,13 @@ export default function CardDescriptions() {
         <div>
             {loading && <div>Loading...</div>}
             {product && (
-                <div className="flex">
-                    <div className="flex justify-center items-center w-1/2">
+                <div className="md:flex">
+                    <div className="flex justify-center items-center md:w-1/2">
                         <img src={product.image} alt={product.title} className="h-48 md:h-96" />
                     </div>
-                    <div className="flex flex-col w-1/2 md:flex md:justify-start items-start">
+                    <div className="flex flex-col md:w-1/2 md:flex md:justify-start md:items-start">
                         <h1 className="text-3xl font-bold pt-3">{product.category}</h1>
-                        <h2 className="text-3xl leading-9 pt-4 md:text-start">{product.title}</h2>
+                        <h2 className="md:text-3xl leading-9 pt-4 md:text-start text-2xl">{product.title}</h2>
                         <p className="font-extrabold text-[18px] pt-3">Rating: {product.rating.rate}</p>
                         <p className="pt-4 md:text-start">{product.description}</p>
                         <div className="flex gap-4 pt-5">
